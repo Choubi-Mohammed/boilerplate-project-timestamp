1e4a9134d4b455ce235cfb04d0549803c8fc8a16
Mohammed Choubi <choubimohammed99@gmail.com>
2024-12-17 15:43:35 +0000

    fix les error

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/api/:date?",(req,res)=>{
     }
   }
   if(isNaN(date.getTime())){
-    return res.json({error :"invald Date"})
+    return res.json({error: "Invalid Date"})
   }
   res.json({
     unix: date.getTime(),
